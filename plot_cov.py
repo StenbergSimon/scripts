b455e2af5e123664c81b6fbc08cf203c6c9587e6
StenbergSimon <simon.stenberg@nmbu.no>
2015-04-10 14:35:29 +0200

    First commit

---
+++ plot_cov.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python 
+from subprocess import Popen, PIPE
+from optparse import OptionParser as opt
+import matplotlib.pyplot as plt
+import os
+from matplotlib.backends.backend_pdf import PdfPages
+
+# Need program genomeCoverageBEd
+
+prsr = opt()
+prsr.add_option("-i", "--input", dest="input_file", metavar="FILE", help="Input BAM file")
+prsr.add_option("-o", "--output", dest="out", default="coverage_plot.pdf", metavar="FILE", help="Output plot [Default:%default]")
+(options, args) = prsr.parse_args()
+
+def getCov(options):
+	COV = []
+	bed = Popen(["genomeCoverageBed", "-d", "-ibam", options.input_file], stdout=PIPE, stderr=PIPE)
+	cut = Popen(["cut", "-f", "3"], stdin=bed.stdout, stdout=PIPE, stderr=PIPE)
+	bed.stdout.close()
+	COV = cut.communicate()[0]
+	bed.wait()
+	return COV
+
+def plotHist(COV, options):
+	plt.hist(COV, bins=50, range=(0,500))
+	plt.title(os.path.basename(options.input_file))	
+	plt.ylabel("Number of bases")
+	plt.xlabel("Coverage")
+	pp = PdfPages(options.out)
+	pp.savefig()
+	pp.close()
+
+COV = ()
+COV = getCov(options)
+COV = COV.split("\n")
+COV = filter(None, COV)
+COV = [int(x) for x in COV]
+plotHist(COV, options)
+
