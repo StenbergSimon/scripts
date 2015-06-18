7f7edb2bda05efff390df3a37247294fb7fb405f
StenbergSimon <simon.stenberg@nmbu.no>
2015-06-18 14:17:08 +0200

    Fixed

--- plot_curves.py
+++ plot_curves.py
@@ -50,15 +50,11 @@ if __name__ == "__main__":
 	if bool(options.random) == True:
 		options.row,options.col = get_random_pos()
 		out = extract_curves(options, dates)
-		for bla in out:
-			for blaha in bla:
-				print type(blaha)
-			
 		out = np.ma.masked_array(out,np.isnan(out))
-		while not (np.sum(out)) > 1:
-			options.row,options.col = get_random_pos()
-			out = extract_curves(options, dates)
-			out = np.ma.masked_array(out,np.isnan(out))
+		#while not (np.sum(out)) > 1:
+		#	options.row,options.col = get_random_pos()
+		#	out = extract_curves(options, dates)
+		#	out = np.ma.masked_array(out,np.isnan(out))
 	else:
 		out = extract_curves(options, dates)
 	
